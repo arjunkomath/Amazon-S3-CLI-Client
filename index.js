e12cd3566b5086644a00be30ff0730b353393c36
Arjun Komath <arjunkomath@gmail.com>
2016-03-12 19:32:25 +0530

    check for aws config

--- index.js
+++ index.js
@@ -7,52 +7,61 @@ var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 var fs = require('fs');
 var mime = require('mime-types');
+var homedir = require('homedir'); 
 
 program
 .arguments('<file>')
 .option('-b, --bucket <bucket>', 'The bucket to upload to')
 .action(function(file) {
 
+
 	try {
-		var fileSize = fs.statSync(file).size;
-		var fileStream = fs.createReadStream(file);
-	} catch(e) {
-		console.log(chalk.bold.red('Oops! No such file or directory'));
-		return false;
+		fs.accessSync(homedir()+'/.aws/credentials', fs.F_OK);
+		try {
+			var fileSize = fs.statSync(file).size;
+			var fileStream = fs.createReadStream(file);
+		} catch(e) {
+			console.log(chalk.bold.red('Oops! No such file or directory'));
+			return false;
+		}
+
+		var file_name = file.split('/').pop();
+		var ext = path.extname(file_name);
+
+		console.log(chalk.cyan('Uploading %s to bucket %s'), file_name, program.bucket);
+
+		var barOpts = {
+			width: 50,
+			total: fileSize
+		};
+		var bar = new ProgressBar(' Uploading [:bar] :percent :etas', barOpts);
+
+		var params = {
+			params: {
+				Bucket: program.bucket, 
+				ACL: 'public-read', 
+				Key: file_name.trim()
+			}
+		};
+
+		if(mime.lookup(file_name)) 
+			params.params.ContentType = mime.lookup(file_name);
+
+		var s3obj = new AWS.S3(params);
+		s3obj.upload({Body: fileStream}).
+		on('httpUploadProgress', function(evt) { 
+			bar.tick(evt.loaded);
+		}).send(function(err, data) {
+			if(err) {
+				console.log(err);
+			} else {
+				console.log(chalk.bold.green('Upload Completed!, '+ data.Location)); 
+			}
+		});
+	} catch (e) {
+		console.log(chalk.bold.red('Oops! AWS config file is missing, Please check https://github.com/arjunkomath/Amazon-S3-CLI-Client#configure'));
 	}
 
-	var file_name = file.split('/').pop();
-	var ext = path.extname(file_name);
-	
-	console.log(chalk.cyan('Uploading %s to bucket %s'), file_name, program.bucket);
 
-	var barOpts = {
-		width: 50,
-		total: fileSize
-	};
-	var bar = new ProgressBar(' Uploading [:bar] :percent :etas', barOpts);
-
-	var params = {
-		params: {
-			Bucket: program.bucket, 
-			ACL: 'public-read', 
-			Key: file_name.trim()
-		}
-	};
-	
-	if(mime.lookup(file_name)) 
-		params.params.ContentType = mime.lookup(file_name);
-
-	var s3obj = new AWS.S3(params);
-	s3obj.upload({Body: fileStream}).
-	on('httpUploadProgress', function(evt) { 
-		bar.tick(evt.loaded);
-	}).send(function(err, data) {
-		if(err) {
-			console.log(err);
-		} else {
-			console.log(chalk.bold.green('Upload Completed!, '+ data.Location)); 
-		}
-	});
 })
 .parse(process.argv);
