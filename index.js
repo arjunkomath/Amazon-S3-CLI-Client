40fe9f5cb645c25677230380c0a26488524acbaa
Arjun Komath <arjunkomath@gmail.com>
2016-03-12 19:54:21 +0530

    config option

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ var s3 = new AWS.S3();
 var fs = require('fs');
 var mime = require('mime-types');
 var homedir = require('homedir'); 
+var co = require('co');
+var prompt = require('co-prompt');
 
 program
+.version('0.0.4')
 .arguments('<file>')
 .option('-b, --bucket <bucket>', 'The bucket to upload to')
 .action(function(file) {
@@ -63,5 +66,32 @@ program
 	}
 
 
-})
-.parse(process.argv);
+});
+
+program
+.command('configure')
+.alias('config')
+.description('Configure Cloud Push')
+.action(function() {
+	co(function *() {
+		var key = yield prompt('IAM Key: ');
+		var secret = yield prompt.password('IAM Secret: ');
+
+		if(!key.trim() || secret.trim()) {
+			console.log(chalk.bold.red('IAM Key and Secret are both mandatory!'));
+			return false;
+		}
+
+		var body = "[default]\n";
+		body += "aws_access_key_id = "+key+"\n";
+		body += "aws_secret_access_key = "+secret;
+		fs.writeFile(homedir()+'/.aws/credentials', body, function(err) {
+			if(err) {
+				return console.log(err);
+			}
+			console.log("Config file saved");
+		}); 
+	});
+});
+
+program.parse(process.argv);
